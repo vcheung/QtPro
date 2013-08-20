0605b4c10389b0a8163f199384cbb7848904d02a
vcheung <66822415@qq.com>
2013-08-20 17:08:13 +0800

    显示中文

--- TCP(2)/tcpSender/main.cpp
+++ TCP(2)/tcpSender/main.cpp
@@ -1,8 +1,10 @@
 #include "widget.h"
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
+    QTextCodec::setCodecForTr(QTextCodec::codecForLocale());
     QApplication a(argc, argv);
     Widget w;
     w.show();
