4c102ec5ab6a1c39f5a795e5cfed840a052db80f
vcheung <66822415@qq.com>
2013-08-20 17:09:11 +0800

    tcpSender_@tcp(2)_OK

---
+++ TCP(2)/build-tcpSender-kit在本地运行-Debug/ui_widget.h
@@ -0,0 +1,92 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created: Tue Aug 20 17:05:37 2013
+**      by: Qt User Interface Compiler version 4.7.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QLineEdit>
+#include <QtGui/QProgressBar>
+#include <QtGui/QPushButton>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QLabel *label;
+    QLabel *label_2;
+    QLineEdit *hostLineEdit;
+    QLineEdit *portLineEdit;
+    QProgressBar *clientProgressBar;
+    QLabel *clientStatusLabel;
+    QPushButton *openButton;
+    QPushButton *sendButton;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QString::fromUtf8("Widget"));
+        Widget->resize(400, 300);
+        label = new QLabel(Widget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(50, 60, 54, 12));
+        label_2 = new QLabel(Widget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(50, 110, 54, 12));
+        hostLineEdit = new QLineEdit(Widget);
+        hostLineEdit->setObjectName(QString::fromUtf8("hostLineEdit"));
+        hostLineEdit->setGeometry(QRect(110, 60, 113, 20));
+        portLineEdit = new QLineEdit(Widget);
+        portLineEdit->setObjectName(QString::fromUtf8("portLineEdit"));
+        portLineEdit->setGeometry(QRect(110, 100, 113, 20));
+        clientProgressBar = new QProgressBar(Widget);
+        clientProgressBar->setObjectName(QString::fromUtf8("clientProgressBar"));
+        clientProgressBar->setGeometry(QRect(50, 150, 231, 23));
+        clientProgressBar->setValue(0);
+        clientStatusLabel = new QLabel(Widget);
+        clientStatusLabel->setObjectName(QString::fromUtf8("clientStatusLabel"));
+        clientStatusLabel->setGeometry(QRect(50, 200, 201, 16));
+        openButton = new QPushButton(Widget);
+        openButton->setObjectName(QString::fromUtf8("openButton"));
+        openButton->setGeometry(QRect(50, 230, 75, 23));
+        sendButton = new QPushButton(Widget);
+        sendButton->setObjectName(QString::fromUtf8("sendButton"));
+        sendButton->setGeometry(QRect(180, 230, 75, 23));
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QApplication::translate("Widget", "Widget", 0, QApplication::UnicodeUTF8));
+        label->setText(QApplication::translate("Widget", "\344\270\273\346\234\272\357\274\232", 0, QApplication::UnicodeUTF8));
+        label_2->setText(QApplication::translate("Widget", "\347\253\257\345\217\243\357\274\232", 0, QApplication::UnicodeUTF8));
+        clientStatusLabel->setText(QApplication::translate("Widget", "\347\212\266\346\200\201\357\274\232\347\255\211\345\276\205\346\211\223\345\274\200\346\226\207\344\273\266\357\274\201", 0, QApplication::UnicodeUTF8));
+        openButton->setText(QApplication::translate("Widget", "\346\211\223\345\274\200", 0, QApplication::UnicodeUTF8));
+        sendButton->setText(QApplication::translate("Widget", "\345\217\221\351\200\201", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
--- TCP(2)/tcpSender/widget.cpp
+++ TCP(2)/tcpSender/widget.cpp
@@ -1,14 +1,108 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QFileDialog>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    loadSize = 4*1024;
+    totalBytes = 0;
+    bytesWritten = 0;
+    bytesToWrite = 0;
+    tcpClient = new QTcpSocket(this);
+    connect(tcpClient,SIGNAL(connected()),this,SLOT(startTransfer()));  //当连接服务器成功时，发出connected()信号，我们开始传送文件
+    connect(tcpClient,SIGNAL(bytesWritten(qint64)),this, SLOT(updateClientProgress(qint64)));     //当有数据发送成功时，我们更新进度条
+    connect(tcpClient,SIGNAL(error(QAbstractSocket::SocketError)),this,SLOT(displayError(QAbstractSocket::SocketError)));
+    ui->sendButton->setEnabled(false);     //开始使”发送“按钮不可用
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::openFile()   //打开文件
+{
+    fileName = QFileDialog::getOpenFileName(this);
+    if(!fileName.isEmpty())
+    {
+        ui->sendButton->setEnabled(true);
+        ui->clientStatusLabel->setText(tr("打开文件 %1 成功!").arg(fileName));
+    }
+}
+
+void Widget::send()   //连接到服务器，执行发送
+{
+    ui->sendButton->setEnabled(false);
+    bytesWritten = 0;     //初始化已发送字节为0
+    ui->clientStatusLabel->setText(tr("连接中……"));
+    tcpClient->connectToHost(ui->hostLineEdit->text(),ui->portLineEdit->text().toInt());    //连接
+}
+
+void Widget::startTransfer()  //实现文件大小等信息的发送
+{
+    localFile = new QFile(fileName);
+    if(!localFile->open(QFile::ReadOnly))
+    {
+        qDebug() << "open file error!";
+        return;
+    }
+    totalBytes = localFile->size();     //文件总大小
+    QDataStream sendOut(&outBlock,QIODevice::WriteOnly);
+    sendOut.setVersion(QDataStream::Qt_4_6);
+    QString currentFileName = fileName.right(fileName.size() - fileName.lastIndexOf('/')-1);
+    sendOut << qint64(0) << qint64(0) << currentFileName;     //依次写入总大小信息空间，文件名大小信息空间，文件名
+    totalBytes += outBlock.size();     //这里的总大小是文件名大小等信息和实际文件大小的总和
+    sendOut.device()->seek(0);
+    sendOut<<totalBytes<<qint64((outBlock.size() - sizeof(qint64)*2));     //返回outBolock的开始，用实际的大小信息代替两个qint64(0)空间
+    bytesToWrite = totalBytes - tcpClient->write(outBlock);     //发送完头数据后剩余数据的大小
+    ui->clientStatusLabel->setText(tr("已连接"));
+    outBlock.resize(0);
+}
+
+void Widget::updateClientProgress(qint64 numBytes) //更新进度条，实现文件的传送
+{
+    bytesWritten += (int)numBytes;     //已经发送数据的大小
+    if(bytesToWrite > 0) //如果已经发送了数据
+    {
+        outBlock = localFile->read(qMin(bytesToWrite,loadSize));
+        //每次发送loadSize大小的数据，这里设置为4KB，如果剩余的数据不足4KB，
+        //就发送剩余数据的大小
+        bytesToWrite -= (int)tcpClient->write(outBlock);        //发送完一次数据后还剩余数据的大小
+        outBlock.resize(0);         //清空发送缓冲区
+    }
+    else
+    {
+        localFile->close(); //如果没有发送任何数据，则关闭文件
+    }
+    ui->clientProgressBar->setMaximum(totalBytes);
+    ui->clientProgressBar->setValue(bytesWritten);     //更新进度条
+    if(bytesWritten == totalBytes) //发送完毕
+    {
+        ui->clientStatusLabel->setText(tr("传送文件 %1 成功").arg(fileName));
+        localFile->close();
+        tcpClient->close();
+    }
+}
+
+void Widget::displayError(QAbstractSocket::SocketError) //显示错误
+{
+    qDebug() << tcpClient->errorString();
+    tcpClient->close();
+    ui->clientProgressBar->reset();
+    ui->clientStatusLabel->setText(tr("客户端就绪"));
+    ui->sendButton->setEnabled(true);
+}
+
+
+void Widget::on_openButton_clicked()
+{
+    openFile();
+}
+
+void Widget::on_sendButton_clicked()
+{
+    send();
 }
